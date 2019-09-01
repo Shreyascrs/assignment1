e3fce034aef3fe86ec91cf027f8c337b85b69b88
shreyascr <shrs601@gmail.com>
2019-09-01 19:38:10 +0530

    [shreyas] Add. Bill with paidBy friend and with description Total amount and return Nonzero

--- src/main/java/com/thoughtworks/com/bootcamp/Bill.java
+++ src/main/java/com/thoughtworks/com/bootcamp/Bill.java
@@ -3,11 +3,13 @@ package com.thoughtworks.com.bootcamp;
 public class Bill {
     double totalAmount;
     Friend paidBy;
+    String description;
 
 
-    public Bill(double totalAmount, Friend friend) {
+    public Bill(double totalAmount, Friend friend,String description) {
         this.totalAmount = totalAmount;
         this.paidBy = friend;
+        this.description=description;
     }
 
     public double settelment() {
--- src/test/java/com/thoughtworks/com/bootcamp/TestSettelment.java
+++ src/test/java/com/thoughtworks/com/bootcamp/TestSettelment.java
@@ -9,28 +9,34 @@ public class TestSettelment {
     @Test
     void ShouldAddBillWithZeroTotalAmount_WhenSettelmentcalled_thenRetuernZero() {
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(0.0, friend);
+        Bill bill = new Bill(0.0, friend, "Auto");
         assertEquals(0.0, bill.settelment());
     }
 
     @Test
     void ShouldAddBillWithNonZeroTotalAmount_WhenSettelmentCalled_ThenReturnNonZero() {
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend);
+        Bill bill = new Bill(1.0, friend, "Auto");
         assertEquals(1.0, bill.settelment());
     }
 
     @Test
     void ShouldAddPaidByFriendToaBillAndTotalAmount_WhenSettelmentcalled_thenReturnTotalAmount() {
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend);
+        Bill bill = new Bill(1.0, friend, "Auto");
         assertEquals(1.0, bill.settelment());
     }
+
     @Test
-    void ShouldAddTotalPaidInFriendClass_WhenSettelmentCalled_ThenReturnTotalAmount()
-    {
-        Friend friend=new Friend("ram");
-        Bill bill=new Bill(1.0,friend);
-        assertEquals(1.0,bill.settelment());
+    void ShouldAddTotalPaidInFriendClass_WhenSettelmentCalled_ThenReturnTotalAmount() {
+        Friend friend = new Friend("ram");
+        Bill bill = new Bill(1.0, friend, "Auto");
+        assertEquals(1.0, bill.settelment());
+    }
+
+    @Test
+    void ShouldAddDescriptionToBill_whenSettelment_ThenReturnTotalAmount() {
+        Friend friend = new Friend("ram");
+        Bill bill = new Bill(1.0, friend, "Auto");
     }
 }
