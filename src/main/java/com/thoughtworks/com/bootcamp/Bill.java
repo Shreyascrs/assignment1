9fabfa369586053f8979c23a6bf5f345a7eb0612
shreyascr <shrs601@gmail.com>
2019-09-02 09:00:18 +0530

    [shreyas] Add. Bill with friendList,totalAmount,description and Paid By Friend  and return TotalAmount

--- src/main/java/com/thoughtworks/com/bootcamp/Bill.java
+++ src/main/java/com/thoughtworks/com/bootcamp/Bill.java
@@ -1,15 +1,19 @@
 package com.thoughtworks.com.bootcamp;
 
+import java.util.List;
+
 public class Bill {
     double totalAmount;
     Friend paidBy;
     String description;
 
+    List<Friend> friendsList;
 
-    public Bill(double totalAmount, Friend friend,String description) {
+    public Bill(double totalAmount, Friend friend, String description, List<Friend> friendsList) {
         this.totalAmount = totalAmount;
         this.paidBy = friend;
-        this.description=description;
+        this.description = description;
+        this.friendsList=friendsList;
     }
 
     public double settelment() {
--- src/test/java/com/thoughtworks/com/bootcamp/TestSettelment.java
+++ src/test/java/com/thoughtworks/com/bootcamp/TestSettelment.java
@@ -4,39 +4,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestSettelment {
 
     @Test
     void ShouldAddBillWithZeroTotalAmount_WhenSettelmentcalled_thenRetuernZero() {
+        List<Friend> friendsList=new ArrayList<>();
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(0.0, friend, "Auto");
+        Bill bill = new Bill(0.0, friend, "Auto", friendsList);
         assertEquals(0.0, bill.settelment());
     }
 
     @Test
     void ShouldAddBillWithNonZeroTotalAmount_WhenSettelmentCalled_ThenReturnNonZero() {
+        List<Friend> friendsList=new ArrayList<>();
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend, "Auto");
+        Bill bill = new Bill(1.0, friend, "Auto", friendsList);
         assertEquals(1.0, bill.settelment());
     }
 
     @Test
     void ShouldAddPaidByFriendToaBillAndTotalAmount_WhenSettelmentcalled_thenReturnTotalAmount() {
+        List<Friend> friendsList=new ArrayList<>();
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend, "Auto");
+        Bill bill = new Bill(1.0, friend, "Auto", friendsList);
         assertEquals(1.0, bill.settelment());
     }
 
     @Test
     void ShouldAddTotalPaidInFriendClass_WhenSettelmentCalled_ThenReturnTotalAmount() {
+        List<Friend> friendsList=new ArrayList<>();
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend, "Auto");
+        Bill bill = new Bill(1.0, friend, "Auto", friendsList);
         assertEquals(1.0, bill.settelment());
     }
 
     @Test
     void ShouldAddDescriptionToBill_whenSettelment_ThenReturnTotalAmount() {
+        List<Friend> friendsList=new ArrayList<>();
         Friend friend = new Friend("ram");
-        Bill bill = new Bill(1.0, friend, "Auto");
+        Bill bill = new Bill(1.0, friend, "Auto", friendsList);
+    }
+
+    @Test
+    void ShouldAddListWithNoFriend_WhenSettelment_ThenReturnTotalAmount() {
+        List<Friend> friendsList=new ArrayList<>();
+        Friend ram=new Friend("ram");
+        Friend lakshman=new Friend("lakshman");
+        friendsList.add(ram);
+        friendsList.add(lakshman);
+        Bill bill=new Bill(1.0,ram,"Hotel",friendsList);
+
     }
 }
